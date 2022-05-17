114699b8dff37ac7ee1762aa2f277399219bf11c
Pawan Kumar <singhpawank12@gmail.com>
2022-05-17 13:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from nltk.corpus import wordnet
+import re
+from nltk.stem import WordNetLemmatizer
 import numpy as np
 import pickle
 import streamlit as st
@@ -7,19 +10,19 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 stop_nltk = stopwords.words("english")
 stop_nltk.remove('not')
-stop_words = stop_nltk + list(punctuation) + ["..."] + ["phone", "mobile","lenovo","k8","product","note","amazon", "got", "n't","'s"] + ["...."] + [".."]
+stop_words = stop_nltk + list(punctuation) + ["..."] + ["phone", "mobile", "lenovo",
+                                                        "k8", "product", "note", "amazon", "got", "n't", "'s"] + ["...."] + [".."]
 
-from nltk.stem import WordNetLemmatizer
 lemm = WordNetLemmatizer()
-import re
-from nltk.corpus import wordnet
+
 
 def clean_txtlemma(sent):
-    sent = re.sub(r"[^\w\s]+"," ",sent)
+    sent = re.sub(r"[^\w\s]+", " ", sent)
     tokens = word_tokenize(sent.lower())
-    lemmatized = [lemm.lemmatize(term, pos = 'v') for term in tokens if term not in stop_nltk and len(term) > 1] 
+    lemmatized = [lemm.lemmatize(
+        term, pos='v') for term in tokens if term not in stop_nltk and len(term) > 1]
     res = " ".join(lemmatized)
-    res = re.sub("n't","not",res)#replacing n't with not
+    res = re.sub("n't", "not", res)  # replacing n't with not
     words = word_tokenize(res)
     new_words = []
     temp_word = ''
@@ -31,7 +34,8 @@ def clean_txtlemma(sent):
             for syn in wordnet.synsets(word):
                 for s in syn.lemmas():
                     for a in s.antonyms():
-                        antonyms.append(a.name())#replacing word following not with its antonymn
+                        # replacing word following not with its antonymn
+                        antonyms.append(a.name())
             if len(antonyms) >= 1:
                 word = antonyms[0]
             else:
@@ -39,22 +43,29 @@ def clean_txtlemma(sent):
             temp_word = ''
         if word != 'not':
             new_words.append(word)
-    new_words = [word for word in new_words if word not in stop_words and len(word) >= 2]
+    new_words = [
+        word for word in new_words if word not in stop_words and len(word) >= 2]
     res = ' '.join(new_words)
     return res
-loaded_model = pickle.load(open('trained_model.sav','rb'))
+
+
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
+
 def prediction(input_data):
     # input_data = ['Without uniform','Overspeeding']
     prediction = loaded_model.predict(input_data)
     print(prediction)
     return prediction[0]
+
+
 def main():
     st.title('Prediction App')
     offence = st.text_input('Review')
     res = ''
     if st.button('Check Sentiment'):
         res = prediction([offence])
-        if res==1:
+        if res == 1:
             st.success("Sentiment: Positive")
         else:
             st.markdown('''
@@ -64,6 +75,8 @@ def main():
             }
             </style>
             ''', unsafe_allow_html=True)
-            st.success("Sentiment: Negative")         
+            st.success("Sentiment: Negative")
+
+
 if __name__ == '__main__':
     main()
